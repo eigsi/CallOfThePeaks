17034fb00e56584b354653518184893309231d75
Louis_Lagrange <58422312+Louis-Lagrange-2609@users.noreply.github.com>
2024-12-03 16:30:04 +0100

    yay

--- Assets/Scripts/2D/Enemy/Boss/Boss.cs
+++ Assets/Scripts/2D/Enemy/Boss/Boss.cs
@@ -8,7 +8,29 @@ public class Boss : MonoBehaviour
 	public Transform player;
 
 	public bool isFlipped = false;
+	private Rigidbody2D bossRigidbody = null;
 
+
+	[Header("Boss")]
+	[Tooltip("Boss jump force.")]
+	public float jumpPower = 1.0f;
+
+	[Header("References")]
+    [Tooltip("The layers which are considered \"Ground\".")]
+    public LayerMask groundLayers = new LayerMask();
+	[Tooltip("The ground check component used to test whether this enemy has hit a wall to the right.")]
+    public Collider2D wallTestFront;
+
+
+	private void Start()
+	{
+		bossRigidbody = GetComponent<Rigidbody2D>();
+		if (bossRigidbody == null)
+		{
+			Debug.LogError("Rigidbody2D n'est pas attaché à " + gameObject.name);
+		}
+	}
+	
 	public void LookAtPlayer()
 	{
 		Vector3 flipped = transform.localScale;
@@ -27,5 +49,22 @@ public class Boss : MonoBehaviour
 			isFlipped = true;
 		}
 	}
+	public void Update()
+	{
+		TestWall(wallTestFront);
+	}
+
+	protected virtual void TestWall(Collider2D wallTestFront)
+    {
+        if (IsInLayerMask(wallTestFront.gameObject, groundLayers))
+        {
+            bossRigidbody.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+	private bool IsInLayerMask(GameObject obj, LayerMask layerMask)
+    {
+        return (layerMask.value & (1 << obj.layer)) != 0;
+    }
 
 }
